c052cf2ad6205e8cb5ca3e59269e07db7cf20da7
Yan Peng <yppe@amazon.com>
2024-12-17 01:03:41 +0000

    Simplifying byte list and use bytes64 to match with symbolic execution

--- arm/proofs/ldst_test.ml
+++ arm/proofs/ldst_test.ml
@@ -12,10 +12,7 @@ loadt "arm/proofs/base.ml";;
 (* ld1, one register, SIMD, 64-bit, immediate offset, post-index *)
 (* ld1 v0[0], [x0], #8 *)
 let LD1_1_SIMD_64_IMM_POST = define_word_list "LD1_1_SIMD_64_IMM_POST"
-  `(CONS ((word 0x00):8 word) 
-     (CONS ((word 0x70):8 word) 
-       (CONS ((word 0xDF):8 word) 
-         (CONS ((word 0x0C):8 word) []))))`;;
+  `[word 0x00; word 0x70; word 0xDF; word 0x0C]:byte list`;;
 
 let LD1_1_SIMD_64_IMM_POST_EXEC = ARM_MK_EXEC_RULE LD1_1_SIMD_64_IMM_POST;;
 
@@ -27,16 +24,19 @@ let LD1_1_SIMD_64_IMM_POST_TEST = time prove
              (\s. aligned_bytes_loaded s (word pc) LD1_1_SIMD_64_IMM_POST /\
                   read PC s = word pc /\
                   C_ARGUMENTS [x] s /\
-                  read (memory :> bytes(x, 8)) s = val a)
+                  read (memory :> (bytes64 x)) s = a)
              (\s. read PC s = word (pc + 0x04) /\
-                  read (DREG' (word 0:5 word)) s = a)
+                  read Q0 s = (word_zx:int64->int128) a)
              (MAYCHANGE [PC; X0],,
               MAYCHANGE [Q0])`,
   MAP_EVERY X_GEN_TAC [`x:int64`; `a:int64`; `pc:num`] THEN
   REWRITE_TAC[C_ARGUMENTS] THEN
   DISCH_THEN(REPEAT_TCL CONJUNCTS_THEN ASSUME_TAC) THEN
   ENSURES_INIT_TAC "s0" THEN
+
   ARM_ACCSTEPS_TAC LD1_1_SIMD_64_IMM_POST_EXEC [] [1] THEN
+
   ENSURES_FINAL_STATE_TAC THEN
   ASM_REWRITE_TAC[]
   );;
+
