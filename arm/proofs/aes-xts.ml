d476108116815b4ea925c41f88b86cd577437442
Yan Peng <yppe@amazon.com>
2025-02-14 01:00:34 +0000

    More experiments

---
+++ arm/proofs/aes-xts.ml
@@ -0,0 +1,8 @@
+(*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
+ *)
+
+needs "arm/proofs/base.ml";;
+
+print_literal_from_elf "arm/aes-xts/aes-xts-armv8.o";;
