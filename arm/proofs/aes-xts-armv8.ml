b246573513ccf1c5ab04a98c1c1e3b8dbc123470
Nevine Ebeid <nebeid@amazon.com>
2025-03-20 17:21:50 -0400

    enable stopping at first error.

--- arm/proofs/aes-xts-armv8.ml
+++ arm/proofs/aes-xts-armv8.ml
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
  *)
 
+use_file_raise_failure := true;;
+
 needs "arm/proofs/base.ml";;
 
 print_literal_from_elf "arm/aes-xts/aes-xts-armv8.o";;
