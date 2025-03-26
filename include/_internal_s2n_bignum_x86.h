597ff3a3c64f731746ed2c00745e00bd7f1d2e87
John Harrison <jargh@amazon.com>
2025-03-26 10:52:37 -0700

    Regenerate AT&T forms of CFI-enabled code
    This involves trifurcating the "_internal_s2n_bignum.h" header
    into three different versions with _arm, _x86 and _x86_att suffixes.
    This is useful in the _x86 and _x86_att cases to distinguish the
    definitions of the CFI adjustment macros, and for _arm to eliminate
    irrelevant CET tests.

--- include/_internal_s2n_bignum_x86.h
+++ include/_internal_s2n_bignum_x86.h
@@ -1,4 +1,3 @@
-
 #ifdef __APPLE__
 #   define S2N_BN_SYMBOL(NAME) _##NAME
 #else
@@ -34,32 +33,10 @@
 #define _CET_ENDBR .byte 0xf3,0x0f,0x1e,0xfa
 #endif
 
-// Variants of standard instructions with CFI (call frame information)
-// annotations included. Two are common to x86 and ARM:
+// Variants of instructions including CFI (call frame information) annotations
 
 #define CFI_START .cfi_startproc
 #define CFI_RET ret ; .cfi_endproc
-
-// These are ARM-specific
-
-#define CFI_BL(target) bl target
-
-#define CFI_PUSH2(lo,hi) stp     lo, hi, [sp, #-16]! ; .cfi_adjust_cfa_offset 16 ; .cfi_rel_offset lo, 0 ; .cfi_rel_offset hi, 8
-#define CFI_PUSH1Z(reg) stp     reg, xzr, [sp, #-16]! ; .cfi_adjust_cfa_offset 16 ; .cfi_rel_offset reg, 0
-
-#define CFI_POP2(lo,hi) ldp     lo, hi, [sp], #16 ; .cfi_adjust_cfa_offset (-16) ; .cfi_restore lo ; .cfi_restore hi
-#define CFI_POP1Z(reg) ldp     reg, xzr, [sp], #16 ; .cfi_adjust_cfa_offset (-16) ; .cfi_restore reg
-
-#define CFI_STACKSAVE2(lo,hi,offset) stp     lo, hi, [sp, #(offset)] ; .cfi_rel_offset lo, offset ; .cfi_rel_offset hi, offset+8
-#define CFI_STACKSAVE1Z(reg,offset) stp     reg, xzr, [sp, #(offset)] ; .cfi_rel_offset reg, offset
-
-#define CFI_STACKLOAD2(lo,hi,offset) ldp     lo, hi, [sp, #(offset)] ; .cfi_restore lo ; .cfi_restore hi
-#define CFI_STACKLOAD1Z(reg,offset) ldp     reg, xzr, [sp, #(offset)] ; .cfi_restore reg
-
-#define CFI_INC_SP(offset) add     sp, sp, #(offset) ; .cfi_adjust_cfa_offset (-(offset))
-#define CFI_DEC_SP(offset) sub     sp, sp, #(offset) ; .cfi_adjust_cfa_offset (offset)
-
-// These are x86-specific
 
 #define CFI_CALL(target) call    target
 
