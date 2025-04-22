bdbe0d8f58f4fd1f41b3c738e43482027e712764
Torben Hansen <50673096+torben-hansen@users.noreply.github.com>
2025-04-22 09:59:27 -0700

    Yet another input

--- include/s2n-bignum.h
+++ include/s2n-bignum.h
@@ -520,7 +520,7 @@ extern void bignum_moddouble (uint64_t k, uint64_t *z, const uint64_t *x, const 
 
 // Modular exponentiation for arbitrary odd modulus, z := (a^p) mod m
 // Inputs a[k], p[k], m[k]; output z[k], temporary buffer t[>=3*k]
-extern void bignum_modexp(uint64_t k,uint64_t *z, const uint64_t *a,uint64_t *p,const uint64_t *m,uint64_t *t);
+extern void bignum_modexp(uint64_t k,uint64_t *z, const uint64_t *a,const uint64_t *p,const uint64_t *m,uint64_t *t);
 
 // Compute "modification" constant z := 2^{64k} mod m
 // Input m[k]; output z[k]; temporary buffer t[>=k]
