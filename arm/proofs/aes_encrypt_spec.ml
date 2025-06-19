9b134b2ff2e561fd2ab6005037ce4b2306725665
Nevine Ebeid <nebeid@amazon.com>
2025-06-19 14:00:02 -0400

    The spec test vector succeeds after converting the inputs to little endian representation.

--- arm/proofs/aes_encrypt_spec.ml
+++ arm/proofs/aes_encrypt_spec.ml
@@ -1,6 +1,11 @@
 use_file_raise_failure := true;;
 needs "common/aes.ml";;
 
+let pp_print_num fmt tm =
+  let n = dest_numeral tm in
+  pp_print_string fmt (string_of_num_hex n) in
+install_user_printer("pp_print_num",pp_print_num);;
+
 (*
 Cipher(byte in[4*Nb], byte out[4*Nb], word w[Nb*(Nr+1)])
 
@@ -73,13 +78,15 @@ let aes256_encrypt = new_definition
 let PRINT_TERM_CONV t = Format.printf "%a\n" pp_print_qterm t; ALL_CONV t;;
 
 let AESENC_ROUND_HELPER_CONV =
-  PRINT_TERM_CONV THENC
+    PRINT_TERM_CONV THENC
   REWRITE_CONV [aes256_encrypt_round] THENC
-  PRINT_TERM_CONV THENC
+  (*PRINT_TERM_CONV THENC*)
   AES_SHIFT_ROWS_CONV THENC
-  PRINT_TERM_CONV THENC
+  (*PRINT_TERM_CONV THENC*)
   AES_SUB_BYTES_CONV THENC
+    PRINT_TERM_CONV THENC
   AES_MIX_COLUMNS_CONV THENC
+    PRINT_TERM_CONV THENC
   DEPTH_CONV (WORD_RED_CONV ORELSEC NUM_RED_CONV);;
 
 let AESENC_REDUCE_CONV tm =
@@ -95,35 +102,61 @@ prove(`aes256_encrypt_round (word 0x7b5b54657374566563746f725d53475d)
        word 0xa8311c2f9fdba3c58b104b58ded7e595`,
        CONV_TAC(LAND_CONV AESENC_REDUCE_CONV) THEN REFL_TAC);;
 
+prove(`aes256_encrypt_round (word 0xAB60EEF6E1D04EC228EE8A3BF255FC0B)
+              (word 0xF4DF1409A310982DD708613B072C351F) =
+       word 0x416EAD9670A2C6D71CFE3FCCB03F10D9`,
+       CONV_TAC(LAND_CONV AESENC_REDUCE_CONV) THEN REFL_TAC);;
+
 (*
 https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/AES_Core256.pdf
 has the same test vector as
-https://www.intel.com/content/dam/doc/white-paper/advanced-encryption-standard-new-instructions-set-paper.pdf:
+https://www.intel.com/content/dam/doc/white-paper/advanced-encryption-standard-new-instructions-set-paper.pdf
+The vector values appear in the code in reverse byte order to be little endian.
+
 AES256_TEST_KEY[] =
-603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4
+603DEB1015CA71BE2B73AEF0857D77811F352C073B6108D72D9810A30914DFF4
 AES_TEST_VECTOR[] =
-6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be66c3710
+6BC1BEE22E409F96E93D7E117393172A
 ECB256_EXPECTED[] =
-f3eed1bdb5d2a03c064b5a7e3db181f8591ccb10d410ed26dc5ba74a31362870b6ed21b99ca6f4f9f153e7b1beafed1d23304b7a39f9f3ff067d8d8f9e24ecc7
+F3EED1BDB5D2A03C064B5A7E3DB181F8
+
+Round keys in big endian generated from the key via a python code
+Round  0: 603DEB1015CA71BE2B73AEF0857D7781
+Round  1: 1F352C073B6108D72D9810A30914DFF4
+Round  2: 9BA354118E6925AFA51A8B5F2067FCDE
+Round  3: A8B09C1A93D194CDBE49846EB75D5B9A
+Round  4: D59AECB85BF3C917FEE94248DE8EBE96
+Round  5: B5A9328A2678A647983122292F6C79B3
+Round  6: 812C81ADDADF48BA24360AF2FAB8B464
+Round  7: 98C5BFC9BEBD198E268C3BA709E04214
+Round  8: 68007BACB2DF331696E939E46C518D80
+Round  9: C814E20476A9FB8A5025C02D59C58239
+Round 10: DE1369676CCC5A71FA2563959674EE15
+Round 11: 5886CA5D2E2F31D77E0AF1FA27CF73C3
+Round 12: 749C47AB18501DDAE2757E4F7401905A
+Round 13: CAFAAAE3E4D59B349ADF6ACEBD10190D
+Round 14: FE4890D1E6188D0B046DF344706C631E
 *)
 
+(* These are the ound keys with the bytes reversed to become little endian *)
 let ROUND_KEYS = new_definition `ROUND_KEYS:int128 list =
-  [ word 0x603DEB1015CA71BE2B73AEF0857D7781
-  ; word 0x1F352C073B6108D72D9810A30914DFF4
-  ; word 0x9BA354118E6925AFA51A8B5F2067FCDE
-  ; word 0xA8B09C1A93D194CDBE49846EB75D5B9A
-  ; word 0xD59AECB85BF3C917FEE94248DE8EBE96
-  ; word 0xB5A9328A2678A647983122292F6C79B3
-  ; word 0x812C81ADDADF48BA24360AF2FAB8B464
-  ; word 0x98C5BFC9BEBD198E268C3BA709E04214
-  ; word 0x68007BACB2DF331696E939E46C518D80
-  ; word 0xC814E20476A9FB8A5025C02D59C58239
-  ; word 0xDE1369676CCC5A71FA2563959674EE15
-  ; word 0x5886CA5D2E2F31D77E0AF1FA27CF73C3
-  ; word 0x749C47AB18501DDAE2757E4F7401905A
-  ; word 0xCAFAAAE3E4D59B349ADF6ACEBD10190D
-  ; word 0xFE4890D1E6188D0B046DF344706C631E
+  [ word 0x81777D85F0AE732BBE71CA1510EB3D60
+  ; word 0xF4DF1409A310982DD708613B072C351F
+  ; word 0xDEFC67205F8B1AA5AF25698E1154A39B
+  ; word 0x9A5B5DB76E8449BECD94D1931A9CB0A8
+  ; word 0x96BE8EDE4842E9FE17C9F35BB8EC9AD5
+  ; word 0xB3796C2F2922319847A678268A32A9B5
+  ; word 0x64B4B8FAF20A3624BA48DFDAAD812C81
+  ; word 0x1442E009A73B8C268E19BDBEC9BFC598
+  ; word 0x808D516CE439E9961633DFB2AC7B0068
+  ; word 0x3982C5592DC025508AFBA97604E214C8
+  ; word 0x15EE7496956325FA715ACC6C676913DE
+  ; word 0xC373CF27FAF10A7ED7312F2E5DCA8658
+  ; word 0x5A9001744F7E75E2DA1D5018AB479C74
+  ; word 0x0D1910BDCE6ADF9A349BD5E4E3AAFACA
+  ; word 0x1E636C7044F36D040B8D18E6D19048FE
   ]`;;
+
 
 let EL_15_128_CLAUSES =
   let pat = `EL n [x0;x1;x2;x3;x4;x5;x6;x7;x8;x9;x10;x11;x12;x13;x14]:128 word` in
@@ -131,7 +164,7 @@ let EL_15_128_CLAUSES =
 
 REWRITE_CONV [ROUND_KEYS]
   `let res0 =
-      word_xor (word 143233380420387077518460912116591433514) (EL 0 ROUND_KEYS) in
+     word_xor (word 143233380420387077518460912116591433514) (EL 0 ROUND_KEYS) in
   let res1 = aes256_encrypt_round res0 (EL 1 ROUND_KEYS) in
   let res2 = aes256_encrypt_round res1 (EL 2 ROUND_KEYS) in
   let res3 = aes256_encrypt_round res2 (EL 3 ROUND_KEYS) in
@@ -152,137 +185,37 @@ REWRITE_CONV [ROUND_KEYS]
 let AESENC_HELPER_CONV =
   PRINT_TERM_CONV THENC
   REWR_CONV aes256_encrypt THENC
-  PRINT_TERM_CONV THENC
+(*  PRINT_TERM_CONV THENC *)
   REWRITE_CONV [ROUND_KEYS] THENC
-  PRINT_TERM_CONV THENC
+(*  PRINT_TERM_CONV THENC *)
   REWRITE_CONV EL_15_128_CLAUSES THENC
-  PRINT_TERM_CONV THENC
-  (*REPEATC let_CONV THENC*)
-  PRINT_TERM_CONV THENC
+(*  PRINT_TERM_CONV THENC *)
+  REPEATC let_CONV THENC
   DEPTH_CONV (WORD_RED_CONV ORELSEC NUM_RED_CONV) THENC
-  ONCE_DEPTH_CONV AESENC_ROUND_HELPER_CONV
+  DEPTH_CONV AESENC_REDUCE_CONV THENC
+  AES_SHIFT_ROWS_CONV THENC
+  AES_SUB_BYTES_CONV THENC
+  PRINT_TERM_CONV THENC
+  DEPTH_CONV (WORD_RED_CONV ORELSEC NUM_RED_CONV)
   ;;
 
 (*
-let AESENC_HELPER_CONV =
-  PRINT_TERM_CONV THENC
-  REWR_CONV aes256_encrypt THENC
-  PRINT_TERM_CONV THENC
-  REWRITE_CONV [ROUND_KEYS] THENC
-  PRINT_TERM_CONV THENC
-  REWRITE_CONV EL_15_128_CLAUSES THENC
-  PRINT_TERM_CONV THENC
-  REPEATC let_CONV THENC
-  PRINT_TERM_CONV THENC
-  DEPTH_CONV (WORD_RED_CONV ORELSEC NUM_RED_CONV) THENC
-  ;;
-evaluates to
-`
-word_xor
-(aes_sub_bytes joined_GF2
-(aes_shift_rows
-(aes256_encrypt_round
- (aes256_encrypt_round
-  (aes256_encrypt_round
-   (aes256_encrypt_round
-    (aes256_encrypt_round
-     (aes256_encrypt_round
-      (aes256_encrypt_round
-       (aes256_encrypt_round
-        (aes256_encrypt_round
-         (aes256_encrypt_round
-          (aes256_encrypt_round
-           (aes256_encrypt_round
-            (aes256_encrypt_round
-             (word_xor (word 143233380420387077518460912116591433514)
-             (word 127927385344373932012120245488993531777))
-            (word 41482152601831359329908363118012325876))
-           (word 206878388847962593259677505409113390302))
-          (word 224227313700546055987490685253953870746))
-         (word 283929978073507663308377981111166221974))
-        (word 241468790472035055121896954896908581299))
-       (word 171701502723078332096379282178375726180))
-      (word 203069427764564296087722130005204943380))
-     (word 138242219980614615854290912811605396864))
-    (word 265954029272361673955751890726720733753))
-   (word 295189406551744036947605646357814373909))
-  (word 117671935837368680518160876205111210947))
- (word 155001899427603818862311956147060445274))
-(word 269805595428433041131718239175312414989))))
-(word 338000693600063263164096359463337616158)`
-*)
-
+(* This proof works and takes about half a minute on an M3 *)
 let tmp = AESENC_HELPER_CONV
   `aes256_encrypt
-    (word 0x6bc1bee22e409f96e93d7e117393172a)
+    (word 0x2A179373117E3DE9969F402EE2BEC16B)
     ROUND_KEYS`;;
-(*
-let AESENC_REDUCE_CONV tm =
-  match tm with
-    Comb(Comb(Const("aes256_encrypt",_),
-         Comb(Const("word",_),state)),
-         Comb(Const("word",_),roundkey))
-    when is_numeral state && is_numeral roundkey -> AESENC_HELPER_CONV tm
-    | _ -> failwith "AESENC_REDUCE_CONV: inapplicable";;
+
+prove(list_mk_comb (`(=):((128)word->(128)word->bool)`,
+      [rand (concl tmp);`(word 0xf881b13d7e5a4b063ca0d2b5bdd1eef3):(128)word`]),
+      REFL_TAC);;
 *)
+
 (*
-let aes256_encrypt = new_definition
-  `aes256_encrypt (block:int128) (key_schedule:int128 list) =
-     let res1 = word_xor block (EL 0 key_schedule) in
-     let res2 = aes_shift_rows res1 in
-     let res3 = aes_sub_bytes joined_GF2 res2 in
-     let res4 = aes_mix_columns res3 in
-     let res5 = word_xor res4 (EL 1 key_schedule) in
-     let res6 = aes_shift_rows res5 in
-     let res7 = aes_sub_bytes joined_GF2 res6 in
-     let res8 = aes_mix_columns res7 in
-     let res9 = word_xor res8 (EL 2 key_schedule) in
-     let res10 = aes_shift_rows res9 in
-     let res11 = aes_sub_bytes joined_GF2 res10 in
-     let res12 = aes_mix_columns res11 in
-     let res13 = word_xor res12 (EL 3 key_schedule) in
-     let res14 = aes_shift_rows res13 in
-     let res15 = aes_sub_bytes joined_GF2 res14 in
-     let res16 = aes_mix_columns res15 in
-     let res17 = word_xor res16 (EL 4 key_schedule) in
-     let res18 = aes_shift_rows res17 in
-     let res19 = aes_sub_bytes joined_GF2 res18 in
-     let res20 = aes_mix_columns res19 in
-     let res21 = word_xor res20 (EL 5 key_schedule) in
-     let res22 = aes_shift_rows res21 in
-     let res23 = aes_sub_bytes joined_GF2 res22 in
-     let res24 = aes_mix_columns res23 in
-     let res25 = word_xor res24 (EL 6 key_schedule) in
-     let res26 = aes_shift_rows res25 in
-     let res27 = aes_sub_bytes joined_GF2 res26 in
-     let res28 = aes_mix_columns res27 in
-     let res29 = word_xor res28 (EL 7 key_schedule) in
-     let res30 = aes_shift_rows res29 in
-     let res31 = aes_sub_bytes joined_GF2 res30 in
-     let res32 = aes_mix_columns res31 in
-     let res33 = word_xor res32 (EL 8 key_schedule) in
-     let res34 = aes_shift_rows res33 in
-     let res35 = aes_sub_bytes joined_GF2 res34 in
-     let res36 = aes_mix_columns res35 in
-     let res37 = word_xor res36 (EL 9 key_schedule) in
-     let res38 = aes_shift_rows res37 in
-     let res39 = aes_sub_bytes joined_GF2 res38 in
-     let res40 = aes_mix_columns res39 in
-     let res41 = word_xor res40 (EL 10 key_schedule) in
-     let res42 = aes_shift_rows res41 in
-     let res43 = aes_sub_bytes joined_GF2 res42 in
-     let res44 = aes_mix_columns res43 in
-     let res45 = word_xor res44 (EL 11 key_schedule) in
-     let res46 = aes_shift_rows res45 in
-     let res47 = aes_sub_bytes joined_GF2 res46 in
-     let res48 = aes_mix_columns res47 in
-     let res49 = word_xor res48 (EL 12 key_schedule) in
-     let res50 = aes_shift_rows res49 in
-     let res51 = aes_sub_bytes joined_GF2 res50 in
-     let res52 = aes_mix_columns res51 in
-     let res53 = word_xor res52 (EL 13 key_schedule) in
-     let res54 = aes_shift_rows res53 in
-     let res55 = aes_sub_bytes joined_GF2 res54 in
-     word_xor res55 (EL 14 key_schedule)
-  `;;
+(* This other form of the proof works as well in half a minute *)
+time prove (`aes256_encrypt
+    (word 0x2A179373117E3DE9969F402EE2BEC16B)
+    ROUND_KEYS = word 0xF881B13D7E5A4B063CA0D2B5BDD1EEF3`,
+    CONV_TAC(LAND_CONV AESENC_HELPER_CONV) THEN REFL_TAC);;
 *)
+
