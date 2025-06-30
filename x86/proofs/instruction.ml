0637b00637a9cc49a7a51056c5f313025d59e9bc
Jake Massimo <jakemas@amazon.com>
2025-06-30 19:47:44 +0000

    Add AVX2 VPMULLD instruction

--- x86/proofs/instruction.ml
+++ x86/proofs/instruction.ml
@@ -307,6 +307,7 @@ let instruction_INDUCTION,instruction_RECURSION = define_type
    | VPADDW operand operand operand
    | VPAND operand operand operand
    | VPMULHW operand operand operand
+   | VPMULLD operand operand operand 
    | VPMULLW operand operand operand
    | VPSRAD operand operand operand
    | VPSRAW operand operand operand
