a80307e4daafd1b2d9d107246113295cee818ede
Jake Massimo <jakemas@amazon.com>
2025-08-20 11:50:44 -0700

    movd issues

--- x86/proofs/x86.ml
+++ x86/proofs/x86.ml
@@ -1919,11 +1919,9 @@ let x86_execute = define
     | MOVD dest src ->
         (match (operand_size dest, operand_size src) with
            (128, 32) -> 
-             let x = read (OPERAND32 src s) s in
-             (OPERAND128_SSE dest s := word_zx x) s
+             x86_MOVD (OPERAND128_SSE dest s) (OPERAND32 src s)
          | (32, 128) -> 
-             let x = read (OPERAND128_SSE src s) s in
-             (OPERAND32 dest s := word_subword x (0,32)) s
+             x86_MOVD (OPERAND32 dest s) (OPERAND128_SSE src s)
          | _ -> (\s'. F)) s
     | MOVSX dest src ->
         (match (operand_size dest,operand_size src) with
