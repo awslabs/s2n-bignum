2dd5fda4c72a67d2da84908d56cff3a564b41567
Jake Massimo <jakemas@amazon.com>
2025-08-21 14:08:59 -0700

    Add AVX2 VPMOVDQA instruction (#270)
    * Add AVX2 VPMOVDQA instruction
    
    * CI fix
    
    * added alignment check
    
    ---------
    
    Co-authored-by: Yan Peng <112029182+pennyannn@users.noreply.github.com>

--- x86/proofs/instruction.ml
+++ x86/proofs/instruction.ml
@@ -304,6 +304,7 @@ let instruction_INDUCTION,instruction_RECURSION = define_type
    | SUB operand operand
    | TEST operand operand
    | TZCNT operand operand
+   | VMOVDQA operand operand
    | VPADDW operand operand operand
    | VPADDD operand operand operand
    | VPAND operand operand operand
