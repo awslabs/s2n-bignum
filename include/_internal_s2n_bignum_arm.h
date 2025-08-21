893a638cceeeae903502af349657600add9c9d5f
John Harrison <jargh@amazon.com>
2025-08-21 16:29:54 -0700

    Avoid semicolon in new CFI macros
    This copies Justin Smith's solution, aleady applied to existing macros
    in 1aaa31382fd70669782594ffb59db2da84762dbc, to ensure standalone
    compilability on Apple platforms without special preprocessing.

--- include/_internal_s2n_bignum_arm.h
+++ include/_internal_s2n_bignum_arm.h
@@ -31,21 +31,21 @@
 // Variants of instructions including CFI (call frame information) annotations
 
 #define CFI_START .cfi_startproc
-#define CFI_RET ret ; .cfi_endproc
+#define CFI_RET ret __LF .cfi_endproc
 
 #define CFI_BL(target) bl target
 
-#define CFI_PUSH2(lo,hi) stp     lo, hi, [sp, #-16]! ; .cfi_adjust_cfa_offset 16 ; .cfi_rel_offset lo, 0 ; .cfi_rel_offset hi, 8
-#define CFI_PUSH1Z(reg) stp     reg, xzr, [sp, #-16]! ; .cfi_adjust_cfa_offset 16 ; .cfi_rel_offset reg, 0
+#define CFI_PUSH2(lo,hi) stp     lo, hi, [sp, #-16]! __LF .cfi_adjust_cfa_offset 16 __LF .cfi_rel_offset lo, 0 __LF .cfi_rel_offset hi, 8
+#define CFI_PUSH1Z(reg) stp     reg, xzr, [sp, #-16]! __LF .cfi_adjust_cfa_offset 16 __LF .cfi_rel_offset reg, 0
 
-#define CFI_POP2(lo,hi) ldp     lo, hi, [sp], #16 ; .cfi_adjust_cfa_offset (-16) ; .cfi_restore lo ; .cfi_restore hi
-#define CFI_POP1Z(reg) ldp     reg, xzr, [sp], #16 ; .cfi_adjust_cfa_offset (-16) ; .cfi_restore reg
+#define CFI_POP2(lo,hi) ldp     lo, hi, [sp], #16 __LF .cfi_adjust_cfa_offset (-16) __LF .cfi_restore lo __LF .cfi_restore hi
+#define CFI_POP1Z(reg) ldp     reg, xzr, [sp], #16 __LF .cfi_adjust_cfa_offset (-16) __LF .cfi_restore reg
 
-#define CFI_STACKSAVE2(lo,hi,offset) stp     lo, hi, [sp, #(offset)] ; .cfi_rel_offset lo, offset ; .cfi_rel_offset hi, offset+8
-#define CFI_STACKSAVE1Z(reg,offset) stp     reg, xzr, [sp, #(offset)] ; .cfi_rel_offset reg, offset
+#define CFI_STACKSAVE2(lo,hi,offset) stp     lo, hi, [sp, #(offset)] __LF .cfi_rel_offset lo, offset __LF .cfi_rel_offset hi, offset+8
+#define CFI_STACKSAVE1Z(reg,offset) stp     reg, xzr, [sp, #(offset)] __LF .cfi_rel_offset reg, offset
 
-#define CFI_STACKLOAD2(lo,hi,offset) ldp     lo, hi, [sp, #(offset)] ; .cfi_restore lo ; .cfi_restore hi
-#define CFI_STACKLOAD1Z(reg,offset) ldp     reg, xzr, [sp, #(offset)] ; .cfi_restore reg
+#define CFI_STACKLOAD2(lo,hi,offset) ldp     lo, hi, [sp, #(offset)] __LF .cfi_restore lo __LF .cfi_restore hi
+#define CFI_STACKLOAD1Z(reg,offset) ldp     reg, xzr, [sp, #(offset)] __LF .cfi_restore reg
 
-#define CFI_INC_SP(offset) add     sp, sp, #(offset) ; .cfi_adjust_cfa_offset (-(offset))
-#define CFI_DEC_SP(offset) sub     sp, sp, #(offset) ; .cfi_adjust_cfa_offset (offset)
+#define CFI_INC_SP(offset) add     sp, sp, #(offset) __LF .cfi_adjust_cfa_offset (-(offset))
+#define CFI_DEC_SP(offset) sub     sp, sp, #(offset) __LF .cfi_adjust_cfa_offset (offset)
