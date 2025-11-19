e335aa8acde73679855f77499b4af14376b77c04
jargh <78765052+jargh@users.noreply.github.com>
2025-11-18 22:24:28 -0800

    Cosimulate simplified versions of x86 memory operations in the code (#311)
    * Force 32-alignment of RSP at start of x86 cosimulator template
    
    This makes it easier to set up cosimulation of memory operations
    like VMOVDQA that depend on this level of alignment. This wasn't
    necessary before because 16-alignment is already the default on
    many platforms.
    
    * Change methodology for cosimulator's x86 instruction extraction
    
    The previous version extracted the instructions from the formal
    versions of the code inside the proof files. This has a few flaws,
    notably the fact that sometimes line breaks separate the actual list
    from the commented instruction, something that for example meant
    that the important MULX instruction was *never* getting cosimulated.
    
    The new version extracts directly from the source code by mangling +
    assembly + objdump. This results in a strictly larger list of
    instructions (2740 in place of 1342 currently). As well as the basic
    "iclasses" list it also sets up a separate list "iclasses_simplemem"
    of memory instructions, corresponding to memory instructions in the
    code but with all relative addresses forced to be [rsp+32]. This
    allows for at least a limited cosimulation of all the basic iforms
    that occur in the code, except for control-flow and stack modification
    (jcc, jmp, call, ret, push, pop).
    
    * Cosimulate simplified versions of x86 memory operations in the code
    
    This supplements the existing memory operation cosimulation, which
    has harnesses to handle general relative addresses. We now also
    cosimulate "simplified" memory operations, which are basically
    the memory iforms extracted from the code with all relative
    addresses forced to be [rsp+32]. Collectively, this gives broader
    coverage of memory operations for additional sanity checking. We
    now select the kind of simulation with probability 1/3 each
    when running the simulator:
    
     - Register-to-register operations
     - Memory operations with general relative address harness
     - Simplified memory operations extracted from the code
    
    * Add nonexecutable stack directive to x86 cosimulator template
    
    This avoids pollution of the simulator output by linker warnings.
    
    * Assume 32-alignment for x86 simple memory operation cosimulation
    
    This is a bit over-conservative since it's really only needed for
    VMOVDQA at the moment, but it keeps things simple.
    
    * Fix issues and follow suggestions raised in reviews
    
    These points were raised in reviews by @aqjune-aws and @pennyannn.
    
    * Extend and comment the additional simple memory checks
    
    * Clarify non-standard encodings in new x86 test cases
    
    Be explicit that a couple of the new test cases are nonstandard
    encodings, i.e. not what assemblers typically produce but still
    valid in hardware. Also add one such more register case for MOVQ.

--- tools/list-x86-insns.sh
+++ tools/list-x86-insns.sh
@@ -1,21 +1,64 @@
 #!/bin/bash
 # This script must be run from the "x86/" directory
+# Choose output file based on input argument (no default)
 
 if [ "$#" -ne 1 ]; then
   echo "list-x86-insns.sh <output.ml>"
 fi
 
-tmp=`mktemp`
-grep '^  0x' proofs/big*ml | grep '(\*' | grep -v '\[' | grep -v '\]' | \
-    grep -vi push | grep -vi pop | grep -wv RET | grep -wiv rsp | \
-    grep -v '\* J' | grep -v Memop | cut -f2 -d: | sort | uniq > $tmp
-
 outfile=$1
 
-echo "let iclasses = [" > "$outfile"
+# Choose slightly different parameters on clang-based Mac OS / ARM setup
 
-while read -r line; do
-  echo "[${line}];" >> "$outfile"
-done <$tmp
+OSTYPE_RESULT=`uname`
 
-echo "];;" >> "$outfile"
+if [ "$OSTYPE_RESULT" = "Darwin" ]; then
+  ASSEMBLE="as -arch x86_64"
+  OBJDUMP="objdump"
+else
+  ASSEMBLE="as"
+  OBJDUMP="objdump --insn-width=16"
+fi
+
+# Concatenate the code from all s2n-bignum assembler source files.
+# (The pattern is more involved than */*.S to avoid "proofs/template.S".)
+# Scrub any data declarations and then assemble, using the Windows
+# ABI versions since in principle they may use strictly more instructions.
+
+for i in [a-oq-z]*/*.S p[235]*/*.S
+do
+  egrep -v '\.quad|\.word' $i | gcc -E -I ../include  -xassembler-with-cpp -DWINDOWS_ABI=1 - >/tmp/source_nodata.S
+  $ASSEMBLE -c /tmp/source_nodata.S -o /tmp/objcode_nodata.o
+  $OBJDUMP -M intel --no-addresses --no-show-raw-insn -d /tmp/objcode_nodata.o
+done  >/tmp/all_disassembly
+
+# Extract the object files and split into register and memory operations
+# Replace memory cell in all memory ops with a fixed [rsp+32]
+# Thus collect 2 lists of instructions we want to handle
+
+grep '^\s' /tmp/all_disassembly | sed -E -e 's/^( |\t)*//' | sed -e 's/\#.*$//' | sort | uniq >/tmp/all_instructions
+grep '\[' /tmp/all_instructions | grep -vi '^lea'  >/tmp/fullmemory_instructions
+(grep -v '\[' /tmp/all_instructions ; grep -i '^lea' /tmp/all_instructions) >/tmp/other_instructions
+echo '.intel_syntax noprefix' >/tmp/register_instructions
+egrep -vi '^(j|call|ret|push|pop)' /tmp/other_instructions | grep -vwi rsp | grep -vwi rip | sort | uniq  >>/tmp/register_instructions
+echo '.intel_syntax noprefix' >/tmp/memory_instructions
+sed -e 's/\[.*\]/MEMORY_CELL/' /tmp/fullmemory_instructions | grep -vwi rsp | grep -vwi rip | sort | uniq | sed -e 's/MEMORY_CELL/[rsp+32]/' >>/tmp/memory_instructions
+
+# Now turn them into the syntax for the simulator OCaml input
+
+echo 'let iclasses_regreg = [' > "$outfile"
+$ASSEMBLE -c /tmp/register_instructions -o /tmp/register.o
+$OBJDUMP -M intel --no-addresses -d /tmp/register.o | grep '^\s' | sed -E -e 's/^( |\t)*/ /' | sed -E -e 's/( |\t)( |\t).*//' >/tmp/register_codings
+sed -E -e 's/([0-9a-f][0-9a-f])/0x\1;/g' /tmp/register_codings | sed -e 's/^ /\[/' | sed -e 's/;$/];/' >/tmp/register_paste1
+grep -v '.intel_syntax'  /tmp/register_instructions | sed -E -e 's/(^.*$)/\(\* \1 \*\)/' >/tmp/register_paste2
+paste -d ' ' /tmp/register_paste1 /tmp/register_paste2 >> "$outfile"
+echo '];;' >> "$outfile"
+echo '' >> "$outfile"
+
+echo 'let iclasses_simplemem = [' >> "$outfile"
+$ASSEMBLE -c /tmp/memory_instructions -o /tmp/memory.o
+$OBJDUMP -M intel --no-addresses -d /tmp/memory.o | grep '^\s' | sed -E -e 's/^( |\t)*/ /' | sed -E -e 's/( |\t)( |\t).*//' >/tmp/memory_codings
+sed -E -e 's/([0-9a-f][0-9a-f])/0x\1;/g' /tmp/memory_codings | sed -e 's/^ /\[/' | sed -e 's/;$/];/' >/tmp/memory_paste1
+grep -v '.intel_syntax'  /tmp/memory_instructions | sed -E -e 's/(^.*$)/\(\* \1 \*\)/' >/tmp/memory_paste2
+paste -d ' ' /tmp/memory_paste1 /tmp/memory_paste2 >> "$outfile"
+echo '];;' >> "$outfile"
