796e2d266dee46c6558c03e4f3fff2bc26abcfd0
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-01-19 13:30:30 +0800

    Arm64: Add vector UMAXV
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- arm/proofs/simulator_iclasses.ml
+++ arm/proofs/simulator_iclasses.ml
@@ -316,6 +316,9 @@ let iclasses =
   (*** UADDLV ***)
   "0x101110xx110000001110xxxxxxxxxx";
 
+  (*** UMAXV ***)
+  "0x101110xx110000101010xxxxxxxxxx";
+
   (*** UMOV (.d, .s) ***)
   "01001110000x1000001111xxxxxxxxxx";
   "00001110000xx100001111xxxxxxxxxx";
