49fa9305c1fe7228b4beb132acfbc7330bb8d770
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-01-30 12:49:34 +0800

    x86: Add VPMULHRSW (Packed Multiply High with Round and Scale)
    Add support for the VPMULHRSW AVX/AVX2 instruction, which computes a rounded
    multiply-high for each 16-bit lane:
    dst[i] = ((src1[i] * src2[i]) >> 14 + 1) >> 1
    
    This instruction is used in ML-KEM compress/decompress AVX2 assembly.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- x86/proofs/x86.ml
+++ x86/proofs/x86.ml
@@ -1789,6 +1789,20 @@ let x86_VPMULDQ = new_definition
         let res:(128)word = simd2 f (word_zx x) (word_zx y) in
         (dest := (word_zx res):N word) s`;;
 
+let x86_VPMULHRSW = new_definition
+  `x86_VPMULHRSW dest src1 src2 (s:x86state) =
+      let (x:N word) = read src1 s
+      and (y:N word) = read src2 s in
+      let f = (\(x:16 word) (y:16 word).
+               let prod:int32 = word_mul (word_sx x) (word_sx y) in
+               word_subword (word_add (word_ushr prod 14) (word 1:int32)) (1,16)) in
+      if dimindex(:N) = 256 then
+        let res:(256)word = simd16 f (word_zx x) (word_zx y) in
+        (dest := (word_zx res):N word) s
+      else
+        let res:(128)word = simd8 f (word_zx x) (word_zx y) in
+        (dest := (word_zx res):N word) s`;;
+
 let x86_VPMULHW = new_definition
   `x86_VPMULHW dest src1 src2 (s:x86state) =
       let (x:N word) = read src1 s
@@ -3116,6 +3130,14 @@ let x86_execute = define
                              (OPERAND256 src2 s)
         | 128 -> x86_VPMULDQ (OPERAND128 dest s) (OPERAND128 src1 s)
                              (OPERAND128 src2 s)) s)) s
+    | VPMULHRSW dest src1 src2 ->
+        (add_load_event src1 s ,, add_load_event src2 s ,,
+         add_store_event dest s ,,
+        (\s. (match operand_size dest with
+          256 -> x86_VPMULHRSW (OPERAND256 dest s) (OPERAND256 src1 s)
+                                (OPERAND256 src2 s)
+        | 128 -> x86_VPMULHRSW (OPERAND128 dest s) (OPERAND128 src1 s)
+                                (OPERAND128 src2 s)) s)) s
     | VPMULHW dest src1 src2 ->
         (add_load_event src1 s ,, add_load_event src2 s ,,
          add_store_event dest s ,,
@@ -4077,6 +4099,7 @@ let x86_VPERMD_ALT = EXPAND_SIMD_RULE x86_VPERMD;;
 let x86_VPERMQ_ALT = EXPAND_SIMD_RULE x86_VPERMQ;;
 let x86_VPERM2I128_ALT = EXPAND_SIMD_RULE x86_VPERM2I128;;
 let x86_VPMULDQ_ALT = EXPAND_SIMD_RULE x86_VPMULDQ;;
+let x86_VPMULHRSW_ALT = EXPAND_SIMD_RULE x86_VPMULHRSW;;
 let x86_VPMULHW_ALT = EXPAND_SIMD_RULE x86_VPMULHW;;
 let x86_VPMULLD_ALT = EXPAND_SIMD_RULE x86_VPMULLD;;
 let x86_VPMULLW_ALT = EXPAND_SIMD_RULE x86_VPMULLW;;
@@ -4114,7 +4137,7 @@ let X86_OPERATION_CLAUSES =
     x86_SAR; x86_SBB_ALT; x86_SET; x86_SHL; x86_SHLD; x86_SHR; x86_SHRD;
     x86_STC; x86_STD; x86_SUB_ALT; x86_TEST; x86_TZCNT; x86_XCHG; x86_XOR;
     (*** AVX2 instructions ***)
-    x86_VPADDD_ALT; x86_VPADDW_ALT; x86_VPMULHW_ALT; x86_VPINSRD; x86_VPINSRQ; x86_VINSERTI128; x86_VEXTRACTI128;
+    x86_VPADDD_ALT; x86_VPADDW_ALT; x86_VPMULHRSW_ALT; x86_VPMULHW_ALT; x86_VPINSRD; x86_VPINSRQ; x86_VINSERTI128; x86_VEXTRACTI128;
     x86_VPEXTRD; x86_VPEXTRQ; x86_VPMULLD_ALT; x86_VPMULLW_ALT; x86_VPSUBD_ALT; x86_VPSUBW_ALT; x86_VPXOR;
     x86_VPAND; x86_VPANDN; x86_VPOR; x86_VPSRAD_ALT; x86_VPSRAW_ALT; x86_VPSRLD_ALT; x86_VPSRLQ_ALT;
     x86_VPSRLW_ALT; x86_VPBROADCASTD_ALT; x86_VPSLLD_ALT; x86_VPSLLQ_ALT; x86_VPSLLW_ALT;
